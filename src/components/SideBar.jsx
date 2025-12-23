080c81ffc4041185c9d9ba01a36fda499449d0c0
Chidexchris <132584045+Chidexchris@users.noreply.github.com>
2025-12-23 06:05:20 -0800

    Rename sidebar.css to Sidebar.css

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import '../assets/style/sidebar.css'
+import '../assets/style/Sidebar.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
